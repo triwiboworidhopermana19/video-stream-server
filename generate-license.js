be318db68db3baa2b800cafdda09d2f92e78c1a2
triwiboworidhopermana <triwiboworidhopermana@gmail.com>
2024-11-21 03:27:34 +0800

    remove scripts

--- generate-license.js
+++
@@ -1,5 +0,0 @@
-this.composeWith(require.resolve('generator-license'), {
-    name: 'Triwibowo Ridho Permana', // (optional) Owner's name
-    email: 'triwiboworidhopermana@gmail.com', // (optional) Owner's email
-    website: 'https://example.com', // (optional) Owner's website
-});
